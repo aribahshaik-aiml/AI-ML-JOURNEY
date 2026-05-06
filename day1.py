1547641e5c20615cbd000dd2d2d2a2b33155f0d5
aribah shaik <aribahshaik44@gmail.com>
2026-05-06 19:32:58 +0530

    Day 1 complete

---
+++ day1.py
@@ -0,0 +1,24 @@
+# Problem 3 - AVERAGE
+# num1 = float(input("enter first number : "))
+# num2 = float(input("enter second number : "))
+# average = (num1 + num2)/2
+# print("The average is : ", average)
+
+# Problem 4 - Simple Interest
+# P = int(input("enter principle number: "))
+# R= int(input("enter rate: "))
+# T = int(input("enter time: "))
+# SI = (P * R * T)/100
+# print("The result is: ", SI)
+
+# Problem - 5 Temperature Converter
+# C = int(input("enter temperature in celsius : "))
+# F = (C * 9/5) + 32
+# print("The result is : ", F)
+
+# Problem 6 - My Introduction
+Name = input("enter your name : ")
+Age = input("enter your age : ")
+City = input("enter your city : ")
+Dreamjob = input("enter your Dream job : ")
+print("This is my introduction : ", Name , Age, City, Dreamjob)
