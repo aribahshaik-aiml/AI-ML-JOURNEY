c318b0a8168d1f6b301ee0c9652d1696138ae0d7
aribah shaik <aribahshaik44@gmail.com>
2026-05-08 17:42:17 +0530

    Day 3 Complete - Lists  & Tuples

---
+++ day3.py
@@ -0,0 +1,36 @@
+#===================================== Day 3 - ASSIGNMENT ===============================================================================
+#Problem 1 - Shopping List
+shopping = []
+shopping.append("clothes")
+shopping.append("sunglasses")
+shopping.append("grocery")
+shopping.append("books")
+shopping.append("makeup")
+
+print(shopping)
+
+#Problem 2 - List Operations
+numbers = [1, 3, 5, 7, 9]
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+print(len(numbers))
+
+#Problem 3 - Remove & Pop
+Fruits = ["apple" , "banana", "kiwi", "mango", "orange"]
+Fruits.remove("kiwi")
+print(Fruits)
+Fruits.pop(0)
+print(Fruits)
+
+#Problem 4 - Insert
+subjects = ["maths", "science", "english"]
+subjects.insert(1, "python")
+print(subjects)
+
+#Problem 5 - Tuple
+Foods = ("biryani", "shawarma", "milkcake", "biryani", "mandi")
+print(Foods.count("biryani"))
+print(Foods.index("milkcake"))
+
