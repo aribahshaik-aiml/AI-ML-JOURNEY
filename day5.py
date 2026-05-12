3255686e376c63bca6484cd213b9a899aa66d21f
aribah shaik <aribahshaik44@gmail.com>
2026-05-12 11:57:19 +0530

    Day 5 Complete - Loops

---
+++ day5.py
@@ -0,0 +1,47 @@
+# =============== FOR LOOP =============================
+# for i in range (1, 6):
+#     print(i)
+
+# ================== WHILE LOOP =========================
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+#===================== BREAK =================
+# for i in range(1, 10):
+#     if i == 5:
+#         break
+#     print(i)
+
+# ============= CONTINUE ===========================
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print(i)
+        
+# =========== PASS ==============================
+# for i in range (1, 6):
+#     if i == 3:
+#         pass
+#     print(i)
+
+# ====================== Day 5 - ASSIGNMENT ============================
+# Print numbers 1 - 10 using while loops
+# i = 1
+# while i <= 11:
+#     print(i)
+#     i +=1
+
+#Print even numbers 1 - 20 using for loop with break when you reach 12
+# for i in range (1, 21):
+#     if(i % 2 == 0):
+#         print(i)
+#         if i == 12:
+#             break
+
+#Print 1-10 using for loop but skip 5 using continue
+for i in range (1, 11):
+    if (i == 5):
+        continue
+    print(i)
